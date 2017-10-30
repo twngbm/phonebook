b950e5997080684cad51cf4c504cc42c04494a83
twngbm <twngbm@gmail.com>
2017-10-30 17:47:51 +0800

    Change strcture of the dataset to reduce the datasize

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,8 +6,7 @@
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
 #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+typedef struct __PHONE_BOOK_DATA {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +16,10 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} pDATA;
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_DATA *pDATA;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
